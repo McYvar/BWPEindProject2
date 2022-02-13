25fa1b1cc968e4503e81e639c66620ea71aafe3d
McYvar <yvarftoorop@hotmail.com>
2022-02-13 19:09:23 +0100

    Even more movement related stuff

--- Assets/Player/Movement.cs
+++ Assets/Player/Movement.cs
@@ -1,10 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Movement : MonoBehaviour
 {
+    #region varaiables and such
     public GameObject cam;
+    bool camFollow = true;
+    Vector3 camVelocity = Vector3.zero;
+    public float camSpeed;
+
     Vector3 up = Vector3.zero,
         right = new Vector3(0, 90, 0),
         down = new Vector3(0, 180, 0),
@@ -14,15 +20,13 @@ public class Movement : MonoBehaviour
     Vector3 nextPos, destination;
 
     float normalSpeed = 5f;
-    float speed;
+    float speed, fallDistance;
+    public float maxFallDistance;
 
-    bool canMove, moving, canJump, directionChange, falling;
+    bool canMove, moving, canJump, directionChange, falling, dead;
     public LayerMask whatIsWall, whatIsFloor;
+    #endregion
 
-    void LateUpdate()
-    {
-        cam.transform.position = transform.position;
-    }
 
     void Start()
     {
@@ -33,11 +37,22 @@ public class Movement : MonoBehaviour
     }
 
 
+    void LateUpdate()
+    {
+        if (camFollow) cam.transform.position = Vector3.SmoothDamp(cam.transform.position, transform.position, ref camVelocity, camSpeed * Time.deltaTime);
+
+        //if (Input.GetKey(KeyCode.Z)) cam.transform.Rotate(0, 1, 0);
+        //if (Input.GetKey(KeyCode.X)) cam.transform.Rotate(0, -1, 0);
+    }
+
+
     void Update()
     {
         Move();
     }
 
+
+    #region Movement
     void Move()
     {
         transform.position = Vector3.MoveTowards(transform.position, destination, speed * Time.deltaTime);
@@ -47,7 +62,7 @@ public class Movement : MonoBehaviour
         GameObject floor = CheckFloor(1.1f);
         if (floor != null) floor.GetComponent<Glow>().timer = 1f;
 
-        if (!moving)
+        if (!moving && !dead)
         {
             if (Input.GetAxisRaw("Vertical") > 0)
             {
@@ -99,55 +114,57 @@ public class Movement : MonoBehaviour
             if (!directionChange)
             {
                 // Jump two spaces forward
-                if (canJump && CheckForward())
+                if (canJump && CheckForward(2.1f))
                 {
                     destination = transform.position + (2 * nextPos);
                     canJump = false;
                     moving = true;
-                    speed = 2 * normalSpeed;
-                    Debug.Log("Jump two spaces forward");
+                    speed = 2f * normalSpeed;
                 }
                 // Jump two spaces forward over one space
-                else if (canJump && CheckForwardUp())
+                else if (canJump && CheckForwardUp(2.1f))
                 {
                     destination = transform.position + (2 * nextPos) + Vector3.up;
                     canJump = false;
                     moving = true;
-                    speed = normalSpeed * 1.5f;
-                    Debug.Log("Jump two spaces forward over one space");
+                    speed = normalSpeed * 2f;
+                }
+                // Jump two spaces forward and two up
+                else if (canJump && CheckForwardUp(1.1f) && CheckForwardDoubleUp(2.1f))
+                {
+                    destination = transform.position + (2 * nextPos) + (2 * Vector3.up);
+                    canJump = false;
+                    moving = true;
+                    speed = normalSpeed * 2f;
                 }
                 // Jump one space forward and two up
-                else if (canJump && CheckForwardDoubleUp())
+                else if (canJump && CheckForwardDoubleUp(1.1f) && CheckRoof())
                 {
                     destination = transform.position + nextPos + (2 * Vector3.up);
                     canJump = false;
                     moving = true;
                     speed = normalSpeed;
-                    Debug.Log("Jump one space forward and two up");
                 }
                 // Move one space forward and down one
-                else if (CheckForwardDown() && CheckForward())
+                else if (!CheckForwardDown() && CheckForward(1.1f))
                 {
                     destination = transform.position + nextPos + Vector3.down;
                     moving = true;
                     speed = normalSpeed;
-                    Debug.Log("Move one space forward and down one");
                 }
                 // Move one space forward
-                else if (CheckForward())
+                else if (CheckForward(1.1f))
                 {
                     destination = transform.position + nextPos;
                     moving = true;
                     speed = normalSpeed;
-                    Debug.Log("Move one space forward");
                 }
                 // Move one space forward and one up
-                else if (CheckForwardUp())
+                else if (CheckForwardUp(1.1f))
                 {
                     destination = transform.position + nextPos + Vector3.up;
                     moving = true;
                     speed = normalSpeed;
-                    Debug.Log("Move one space forward and one up");
                 }
             }
 
@@ -161,6 +178,7 @@ public class Movement : MonoBehaviour
         {
             if (floor != null)
             {
+                fallDistance = 0;
                 Vector3 temp = new Vector3(floor.transform.position.x + 0.5f, floor.transform.position.y + 2f, floor.transform.position.z - 0.5f);
                 destination = temp;
                 speed = Mathf.Infinity;
@@ -170,15 +188,22 @@ public class Movement : MonoBehaviour
             {
                 destination = transform.position + Vector3.down;
                 speed = 30;
+
+                if (CheckFallDistance() > fallDistance) fallDistance = CheckFallDistance();
+                if (fallDistance > maxFallDistance)
+                {
+                    Invoke("DeadAndRespawn", 3);
+                    dead = true;
+                    camFollow = false;
+                }
+                else Debug.Log(fallDistance);
             }
-            Debug.Log(CheckForward());
-            Debug.Log(CheckForwardUp());
-            Debug.Log(CheckForwardDoubleUp());
-            Debug.Log(CheckForwardDown());
         }
     }
+    #endregion
 
 
+    #region Raycast checks
     bool ValidityCheck(Vector3 startingPosition, Vector3 direction, float length, LayerMask mask)
     {
         Ray ray = new Ray(startingPosition, direction);
@@ -188,47 +213,71 @@ public class Movement : MonoBehaviour
 
         if (Physics.Raycast(ray, out hit, length, mask))
         {
-            return true;
+            return false;
         }
-        return false;
+        return true;
     }
 
 
-    bool CheckForward()
+    bool CheckForward(float length)
     {
-        return ValidityCheck(transform.position, transform.forward, 1.1f, whatIsWall);
+        return ValidityCheck(transform.position, transform.forward, length, whatIsWall);
     }
 
 
-    bool CheckForwardUp()
+    bool CheckForwardUp(float length)
     {
-        return ValidityCheck(transform.position + transform.up, transform.forward, 1.1f, whatIsWall);
+        return ValidityCheck(transform.position + transform.up, transform.forward, length, whatIsWall);
     }
 
 
-    bool CheckForwardDoubleUp()
+    bool CheckForwardDoubleUp(float length)
     {
-        return ValidityCheck(transform.position + (transform.up * 2), transform.forward, 1.1f, whatIsWall);
+        return ValidityCheck(transform.position + (transform.up * 2), transform.forward, length, whatIsWall);
     }
 
 
     bool CheckForwardDown()
     {
-        return ValidityCheck(transform.position + transform.forward, -transform.up, 1.1f, whatIsFloor);
+        return ValidityCheck(transform.position + transform.forward - (1.5f * transform.up), -transform.up, 0.6f, whatIsFloor);
+    }
+
+    bool CheckRoof()
+    {
+        return ValidityCheck(transform.position, transform.up, 2.1f, whatIsWall);
+    }
+
+
+    float CheckFallDistance()
+    {
+        Ray ray = new Ray(transform.position, -transform.up);
+        RaycastHit hit;
+
+        if (Physics.Raycast(ray, out hit, 1000, whatIsFloor))
+        {
+            return Vector3.Distance(ray.origin, hit.transform.position);
+        }
+        return 1000;
     }
 
 
     GameObject CheckFloor(float rayLength)
     {
-        Ray ray = new Ray(transform.position, Vector3.down);
+        Ray ray = new Ray(transform.position, -transform.up);
         RaycastHit hit;
 
         Debug.DrawRay(ray.origin, ray.direction, Color.yellow);
 
-        if (Physics.Raycast(ray, out hit, rayLength, whatIsFloor)) {
+        if (Physics.Raycast(ray, out hit, rayLength, whatIsFloor))
+        {
             return hit.transform.gameObject;
         }
         return null;
     }
+    #endregion
 
+    void DeadAndRespawn()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
