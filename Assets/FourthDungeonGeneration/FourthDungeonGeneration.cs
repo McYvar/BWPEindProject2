7e614cea23966b3a836682c332786371525dabf8
McYvar <yvarftoorop@hotmail.com>
2022-03-17 03:19:48 +0100

    Added a very neat begin for a fourth iteration on the dungeon generator!

--- Assets/FourthDungeonGeneration/FourthDungeonGeneration.cs
+++ Assets/FourthDungeonGeneration/FourthDungeonGeneration.cs
@@ -10,6 +10,10 @@ public class FourthDungeonGeneration : MonoBehaviour
     {
         Rooms room = new Rooms(new Vector3(0, 0, 0), new Vector3(0, 0, 15), new Vector3(15, 0, 0), new Vector3(15, 0, 15), dungeon.Count, mat);
         room.CreateRoomTile();
+        room.CreateRoomWall(room.GetRoomLowerLeft(), room.GetRoomUpperLeft(), false, 3f);
+        room.CreateRoomWall(room.GetRoomUpperLeft(), room.GetRoomUpperRight(), false, 1f);
+        room.CreateRoomWall(room.GetRoomLowerRight(), room.GetRoomUpperRight(), false, 3f);
+        room.CreateRoomWall(room.GetRoomLowerLeft(), room.GetRoomLowerRight(), true, 3f);
     }
 
 
@@ -107,57 +111,127 @@ public class Rooms
         Vector3[] vertices = new Vector3[] { GetRoomLowerLeft(), GetRoomUpperLeft(), GetRoomLowerRight(), GetRoomUpperRight() };
         int[] triangles = new int[] { 0, 1, 2, 2, 1, 3 };
 
-        CreateObjectInstructions(vertices, triangles);
+        CreateObjectInstructions(roomObject ,vertices, triangles);
     }
 
     public void CreateRoomWall(Vector3 pointA, Vector3 pointB, bool hasHall, float wallHeigt)
     {
-        Vector3[] vertices;
-        Vector3[] verticesIfHall;
+        Vector3[] vertices = new Vector3[8];
+        Vector3[] verticesIfHall = new Vector3[8];
+        string name;
 
-        if (pointA.x == pointB.x)  // Horizontal wall
+        if (pointA.z == pointB.z)  // Horizontal wall
         {
-            if (pointA.y > GetRoomCenter().y) // Upper wall
+            if (pointA.z > GetRoomCenter().z) // Upper wall
             {
+                Debug.Log("Upper wall");
                 if (hasHall) 
                 {
-                    Vector3[] tempVertLeftWall = { pointA + Vector3.back,                            pointA,                            GetRoomCenterUp() + Vector3.left + Vector3.back,                            GetRoomCenterUp() + Vector3.left,
-                                                   pointA + Vector3.back + (Vector3.up * wallHeigt), pointA + (Vector3.up * wallHeigt), GetRoomCenterUp() + Vector3.left + Vector3.back + (Vector3.up * wallHeigt), GetRoomCenterUp() + Vector3.left + (Vector3.up * wallHeigt) };
+                    Vector3[] tempVertLeftWall = new Vector3[] { pointA + Vector3.back,                            pointA,                            GetRoomCenterUp() + Vector3.left + Vector3.back,                            GetRoomCenterUp() + Vector3.left,
+                                                                 pointA + Vector3.back + (Vector3.up * wallHeigt), pointA + (Vector3.up * wallHeigt), GetRoomCenterUp() + Vector3.left + Vector3.back + (Vector3.up * wallHeigt), GetRoomCenterUp() + Vector3.left + (Vector3.up * wallHeigt) };
 
-                    Vector3[] tempVertRightWall = { GetRoomCenterUp() + Vector3.right + Vector3.back,                            GetRoomCenterUp() + Vector3.right,                            pointB + Vector3.back,                            pointB,
-                                                    GetRoomCenterUp() + Vector3.right + Vector3.back + (Vector3.up * wallHeigt), GetRoomCenterUp() + Vector3.right + (Vector3.up * wallHeigt), pointB + Vector3.back + (Vector3.up * wallHeigt), pointB + (Vector3.up * wallHeigt)} ;
+                    Vector3[] tempVertRightWall = new Vector3[] { GetRoomCenterUp() + Vector3.right + Vector3.back,                            GetRoomCenterUp() + Vector3.right,                            pointB + Vector3.back,                            pointB,
+                                                                  GetRoomCenterUp() + Vector3.right + Vector3.back + (Vector3.up * wallHeigt), GetRoomCenterUp() + Vector3.right + (Vector3.up * wallHeigt), pointB + Vector3.back + (Vector3.up * wallHeigt), pointB + (Vector3.up * wallHeigt)} ;
+                    vertices = tempVertLeftWall;
+                    verticesIfHall = tempVertRightWall;
                 }
                 else
                 {
-                    Vector3[] tempVert = { pointA + Vector3.back,                            pointA,                            pointB + Vector3.back,                            pointB,
-                                           pointA + Vector3.back + (Vector3.up * wallHeigt), pointA + (Vector3.up * wallHeigt), pointB + Vector3.back + (Vector3.up * wallHeigt), pointB + (Vector3.up * wallHeigt) };
+                    Vector3[] tempVert = new Vector3[] { pointA + Vector3.back,                            pointA,                            pointB + Vector3.back,                            pointB,
+                                                         pointA + Vector3.back + (Vector3.up * wallHeigt), pointA + (Vector3.up * wallHeigt), pointB + Vector3.back + (Vector3.up * wallHeigt), pointB + (Vector3.up * wallHeigt) };
                     vertices = tempVert;
+                    verticesIfHall = null;
                 }
+                name = "Upper";
             }
             else // Lower wall
             {
+                Debug.Log("Lower wall");
+                if (hasHall)
+                {
+                    Vector3[] tempVertLeftWall = new Vector3[] { pointA,                            pointA + Vector3.forward,                            GetRoomCenterDown() + Vector3.left,                            GetRoomCenterDown() + Vector3.left + Vector3.forward,
+                                                                 pointA + (Vector3.up * wallHeigt), pointA + Vector3.forward + (Vector3.up * wallHeigt), GetRoomCenterDown() + Vector3.left + (Vector3.up * wallHeigt), GetRoomCenterDown() + Vector3.left + Vector3.forward + (Vector3.up * wallHeigt) };
 
+                    Vector3[] tempVertRightWall = new Vector3[] { GetRoomCenterDown() + Vector3.right,                            GetRoomCenterDown() + Vector3.right + Vector3.forward,                            pointB,                            pointB + Vector3.forward,
+                                                                  GetRoomCenterDown() + Vector3.right + (Vector3.up * wallHeigt), GetRoomCenterDown() + Vector3.right + Vector3.forward + (Vector3.up * wallHeigt), pointB + (Vector3.up * wallHeigt), pointB + Vector3.forward + (Vector3.up * wallHeigt)};
+                    vertices = tempVertLeftWall;
+                    verticesIfHall = tempVertRightWall;
+                }
+                else
+                {
+                    Vector3[] tempVert = new Vector3[] { pointA,                            pointA + Vector3.forward,                            pointB,                            pointB + Vector3.forward,
+                                                         pointA + (Vector3.up * wallHeigt), pointA + Vector3.forward + (Vector3.up * wallHeigt), pointB + (Vector3.up * wallHeigt), pointB + Vector3.forward + (Vector3.up * wallHeigt) };
+                    vertices = tempVert;
+                    verticesIfHall = null;
+                }
+                name = "Lower";
             }
         }
         else // Vertical wall
         {
             if (pointA.x < GetRoomCenter().x) // Left wall
             {
+                Debug.Log("Left wall");
+                if (hasHall)
+                {
+                    Vector3[] tempVertLowerWall = new Vector3[] { pointA,                            GetRoomCenterLeft() + Vector3.back,                            pointA + Vector3.right,                            GetRoomCenterLeft() + Vector3.back + Vector3.right,
+                                                                  pointA + (Vector3.up * wallHeigt), GetRoomCenterLeft() + Vector3.back + (Vector3.up * wallHeigt), pointA + Vector3.right + (Vector3.up * wallHeigt), GetRoomCenterLeft() + Vector3.back + Vector3.right + (Vector3.up * wallHeigt) };
 
+                    Vector3[] tempVertUpperWall = new Vector3[] { GetRoomCenterLeft() + Vector3.forward,                            pointB + Vector3.right,            GetRoomCenterLeft() + Vector3.forward + Vector3.right,                            pointB,            
+                                                                  GetRoomCenterLeft() + Vector3.forward + (Vector3.up * wallHeigt), pointB + (Vector3.up * wallHeigt), GetRoomCenterLeft() + Vector3.forward + Vector3.right + (Vector3.up * wallHeigt), pointB + Vector3.right + (Vector3.up * wallHeigt)};
+                    vertices = tempVertLowerWall;
+                    verticesIfHall = tempVertUpperWall;
+                }
+                else
+                {
+                    Vector3[] tempVert = new Vector3[] { pointA,                            pointB,                            pointA + Vector3.right,                            pointB + Vector3.right,
+                                                         pointA + (Vector3.up * wallHeigt), pointB + (Vector3.up * wallHeigt), pointA + Vector3.right + (Vector3.up * wallHeigt), pointB + Vector3.right + (Vector3.up * wallHeigt) };
+                    vertices = tempVert;
+                    verticesIfHall = null;
+                }
+                name = "Left";
             }
             else // Right wall
             {
+                Debug.Log("Right wall");
+                if (hasHall)
+                {
+                    Vector3[] tempVertLowerWall = new Vector3[] { pointA - Vector3.left,                            GetRoomCenterRight() + Vector3.back + Vector3.left,                            pointA,                            GetRoomCenterRight() + Vector3.back,
+                                                                  pointA - Vector3.left + (Vector3.up * wallHeigt), GetRoomCenterRight() + Vector3.back + Vector3.left + (Vector3.up * wallHeigt), pointA + (Vector3.up * wallHeigt), GetRoomCenterRight() + Vector3.back + (Vector3.up * wallHeigt) };
 
+                    Vector3[] tempVertUpperWall = new Vector3[] { GetRoomCenterRight() + Vector3.forward + Vector3.left,                            pointB + Vector3.left,                            GetRoomCenterRight() + Vector3.forward,                            pointB,
+                                                                  GetRoomCenterRight() + Vector3.forward + Vector3.left + (Vector3.up * wallHeigt), pointB + Vector3.left + (Vector3.up * wallHeigt), GetRoomCenterRight() + Vector3.forward + (Vector3.up * wallHeigt), pointB + (Vector3.up * wallHeigt) };
+                    vertices = tempVertLowerWall;
+                    verticesIfHall = tempVertUpperWall;
+                }
+                else
+                {
+                    Vector3[] tempVert = new Vector3[] { pointA + Vector3.left,                            pointB + Vector3.left,                            pointA,                            pointB,
+                                                         pointA + Vector3.left + (Vector3.up * wallHeigt), pointB + Vector3.left + (Vector3.up * wallHeigt), pointA + (Vector3.up * wallHeigt), pointB + (Vector3.up * wallHeigt) };
+                    vertices = tempVert;
+                    verticesIfHall = null;
+                }
+                name = "Right";
             }
         }
 
-        int[] triangles;
-
+        GameObject obj = new GameObject(name + " wall");
+        obj.layer = 3;
+        obj.transform.parent = roomObject.transform;
+        int[] triangles = { 2, 1, 0, 3, 1, 2, 3, 5, 1, 7, 5, 3, 6, 3, 2, 7, 3, 6, 6, 0, 4, 2, 0, 6, 0, 5, 4, 1, 5, 0, 7, 4, 5, 6, 4, 7 };
+        CreateObjectInstructions(obj, vertices, triangles);
+        if (hasHall)
+        {
+            GameObject objTwo = new GameObject(name + " wall");
+            objTwo.layer = 3;
+            objTwo.transform.parent = roomObject.transform;
+            CreateObjectInstructions(objTwo, verticesIfHall, triangles);
+        }
     }
 
-    private void CreateObjectInstructions(Vector3[] vertices, int[] triangles)
+    private void CreateObjectInstructions(GameObject obj, Vector3[] vertices, int[] triangles)
     {
-        MeshFilter mesh = roomObject.AddComponent<MeshFilter>();
+        MeshFilter mesh = obj.AddComponent<MeshFilter>();
         mesh.mesh.vertices = vertices;
         mesh.mesh.triangles = triangles;
         mesh.mesh.RecalculateNormals();
@@ -169,9 +243,9 @@ public class Rooms
         }
         mesh.mesh.uv = uvs;
 
-        MeshRenderer mr = roomObject.AddComponent<MeshRenderer>();
+        MeshRenderer mr = obj.AddComponent<MeshRenderer>();
         mr.material = floorMat;
 
-        BoxCollider boxCollider = roomObject.AddComponent<BoxCollider>();
+        BoxCollider boxCollider = obj.AddComponent<BoxCollider>();
     }
 }
